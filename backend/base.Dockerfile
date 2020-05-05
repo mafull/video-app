07b55e3581acf4286225119408c327318126c16d
Max Fuller <max.fuller@cmrsurgical.com>
2020-05-05 23:51:16 +0100

    Implement basic video streaming and display in the frontend

--- backend/base.Dockerfile
+++ backend/base.Dockerfile
@@ -16,13 +16,31 @@ RUN apt-get update && \
         g++-9 && \
     update-alternatives \
         --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 \
-        --slave /usr/bin/g++ g++ /usr/bin/g++-9 && \
-    # websocketd
-    apt-get install -y \
+        --slave /usr/bin/g++ g++ /usr/bin/g++-9
+
+# websocketd
+RUN apt-get install -y \
         unzip \
         wget && \
     wget -q -O /tmp/websocketd.zip https://github.com/joewalnes/websocketd/releases/download/v0.3.0/websocketd-0.3.0-linux_amd64.zip && \
     unzip /tmp/websocketd.zip -d /tmp/websocketd && \
     mv /tmp/websocketd/websocketd /usr/bin
 
-# CMD ["/bin/bash"]
+# OpenCV
+# https://docs.opencv.org/master/d7/d9f/tutorial_linux_install.html
+# http://www.codebind.com/linux-tutorials/install-opencv-ubuntu-18-04-lts-c-cpp-linux/
+RUN apt-get install -y \
+        build-essential \
+        cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev \
+        python-dev python-numpy && \
+        #libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev && \
+    cd /opt && \
+    git clone https://github.com/opencv/opencv.git && \
+    git clone https://github.com/opencv/opencv_contrib.git && \
+    cd opencv && \
+    mkdir release && cd release && \
+    cmake BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules .. && \
+    make -j4 && \
+    make install
+
+CMD ["/bin/bash"]
--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -9,6 +9,9 @@
 #include <filesystem>
 #include <string>
 #include <thread>
+#include <opencv2/opencv.hpp>
+
+#include "base64.hpp"
 
 #include "Video.hpp"
 
@@ -22,28 +25,71 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    // const fs::path picsPath = "/usr/pictures";
-    // if (!fs::exists(picsPath) || !fs::is_directory(picsPath)) {
-    //     cerr << "Directory " << picsPath << " could not be found" << endl;
-    //     return 1;
-    // }
+    const fs::path picsPath = "/usr/pictures";
+    if (!fs::exists(picsPath) || !fs::is_directory(picsPath)) {
+        cerr << "Directory " << picsPath << " could not be found" << endl;
+        return 1;
+    }
 
-    // cout << "Listing contents of directory " << picsPath << endl;
-    // for (const auto& entry : fs::directory_iterator(picsPath)) {
-    //     cout << "> (" << (fs::is_directory(entry) ? "DIR" : "FIL") << ")" << entry.path().filename() << endl;
-    // }
+    cout << "Listing contents of directory " << picsPath << endl;
+    bool firstFile = true;
+    for (const auto& entry : fs::directory_iterator(picsPath)) {
+        cout << "> (" << (fs::is_directory(entry) ? "DIR" : "FIL") << ")" << entry.path().filename() << endl;
+        if (!fs::is_directory(entry) && (entry.path().extension() == ".MP4" || entry.path().extension() == ".mp4") && firstFile) {
+            firstFile = false;
+            cout << "Opening " << entry.path() << "..." << endl;
+            cv::VideoCapture cap(entry.path());
+            if (!cap.isOpened()) {
+                cout << "Error opening video stream or file" << endl;
+                return -1;
+            }
+
+            double fps = cap.get(cv::CAP_PROP_FPS) / 0.5;
+            const int waitTimeMs = 1000 / fps;
+            cout << fps << " - " << waitTimeMs << endl;
+
+
+            int counter = 0;
+            while (1) {
+                cout << "started" << endl;
+                cv::Mat frame;
+                cap >> frame;
+
+                if (frame.empty()) break;
+
+                // if (counter++ % 2 > 0) continue;
+
+                std::vector<base64::byte> buf;
+                cv::imencode(".jpg", frame, buf);
+                // auto *encoded = reinterpret_cast<unsigned char*>(buf.data());
+                std::string encodedStr = base64::encode(buf);
+
+                // cout << "frame: " << frame << endl;
+                // cout << "frame: " << frame.reshape(0,1) << endl;
+                cout << "frame:" << encodedStr << endl;
+
+                char c = cv::waitKey(waitTimeMs);
+                if (c == 88) break;
+
+                // break;
+            }
+
+            cap.release();
+        }
+    }
 
     // const Video video("/dir/hello");
     // const Video video2;
 
     // VideoError vidErr = VideoError::NotFound;
     // cout << static_cast<int>(vidErr) << endl;
+    cv::Mat image;
 
     using namespace std::this_thread;
     using namespace std::chrono_literals;
     int counter = 0;
     cout << "Gunna start counting" << std::endl;
-    while (1) {
+    while (counter < 10) {
         cout << "Count: " << counter++ << std::endl;
         sleep_for(1s);
     }
