5a41409ec438dede87ebebeb85eecad32eac12cf
Max Fuller <max.fuller@cmrsurgical.com>
2020-05-05 23:51:48 +0100

    Hello world usage of OpenCV in the app

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -9,6 +9,7 @@
 #include <filesystem>
 #include <string>
 #include <thread>
+#include <opencv2/opencv.hpp>
 
 #include "Video.hpp"
 
@@ -22,22 +23,32 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    // const fs::path picsPath = "/usr/pictures";
-    // if (!fs::exists(picsPath) || !fs::is_directory(picsPath)) {
-    //     cerr << "Directory " << picsPath << " could not be found" << endl;
-    //     return 1;
-    // }
+    const fs::path picsPath = "/usr/pictures";
+    if (!fs::exists(picsPath) || !fs::is_directory(picsPath)) {
+        cerr << "Directory " << picsPath << " could not be found" << endl;
+        return 1;
+    }
 
-    // cout << "Listing contents of directory " << picsPath << endl;
-    // for (const auto& entry : fs::directory_iterator(picsPath)) {
-    //     cout << "> (" << (fs::is_directory(entry) ? "DIR" : "FIL") << ")" << entry.path().filename() << endl;
-    // }
+    cout << "Listing contents of directory " << picsPath << endl;
+    bool firstFile = true;
+    for (const auto& entry : fs::directory_iterator(picsPath)) {
+        if (!fs::is_directory(entry) && entry.path().extension() == ".mp4" && firstFile) {
+            firstFile = false;
+            cv::VideoCapture cap(entry.path().filename());
+            if (!cap.isOpened()) {
+                cout << "Error opening video stream or file" << endl;
+                return -1;
+            }
+        }
+        cout << "> (" << (fs::is_directory(entry) ? "DIR" : "FIL") << ")" << entry.path().filename() << endl;
+    }
 
     // const Video video("/dir/hello");
     // const Video video2;
 
     // VideoError vidErr = VideoError::NotFound;
     // cout << static_cast<int>(vidErr) << endl;
+    cv::Mat image;
 
     using namespace std::this_thread;
     using namespace std::chrono_literals;
